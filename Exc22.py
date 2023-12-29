d8cc0c7f2126a22bbe60029fe14e1b02c4d1d3d0
Kazure <serdartorlak@gmail.com>
2023-12-29 17:03:04 +0300

    Updates

---
+++ Exc22.py
@@ -0,0 +1,25 @@
+def replace_ascii_with_turkish(text):
+    replacements = {
+        "i": "ı",
+        "I": "İ",
+        "g": "ğ",
+        "G": "Ğ",
+        "u": "ü",
+        "U": "Ü",
+        "o": "ö",
+        "O": "Ö",
+        "c": "ç",
+        "C": "Ç",
+    }
+
+    for old, new in replacements.items():
+        text = text.replace(old, new)
+
+    return text
+
+
+# Example usage
+ascii_text = "Istanbul guzel bir sehir."
+turkish_text = replace_ascii_with_turkish(ascii_text)
+
+print(turkish_text)
