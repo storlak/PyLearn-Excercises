a6804f6a4e75a3da7764f97068b40aab49cc9ba5
Kazure <serdartorlak@gmail.com>
2024-01-03 10:51:36 +0300

    Update

--- Exc20.py
+++ Exc20.py
@@ -4,3 +4,15 @@ def square(number):
 
 result = square(3) # instead print(square(3)).
 print(result)
+
+
+def change_list(data):
+    data = ["new"]
+    
+    
+languages = ["C##", "Python", "Java"]
+
+print(languages)
+change_list(languages)
+print(languages)
+    
