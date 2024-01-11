1bfdc29e0a6cc502d452a6a4a319a5356f12741e
Kazure <serdartorlak@gmail.com>
2024-01-11 18:05:49 +0300

    Updates

--- Exc0.py
+++ Exc0.py
@@ -7,5 +7,5 @@ c, d = 5, 10
 print(c, d)
 
 # to change color
-print("\033[96mThis is a different color") # changing color
-print("\033[91mThis is a different color") # changing color
+print("\033[96mThis is a different color")  # changing color
+print("\033[91mThis is a different color")  # changing color
