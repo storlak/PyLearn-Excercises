fec7111f0f14856b3fe338fdcd04e165167fc5c7
Kazure <serdartorlak@gmail.com>
2024-01-11 12:10:24 +0300

    if statment Exc24

---
+++ Exc24.py
@@ -0,0 +1,20 @@
+age = int(input("Enter your age: "))
+eligible = "Eligible for a credit card."
+not_eligible = "You do not meet the requirements."
+
+if age >= 18 and age < 80:
+    print(eligible)
+elif age > 80:
+    health_cert_num = input("Enter your health certificate number: ")
+
+    try:
+        health_cert_num = int(health_cert_num)
+        if 0 < health_cert_num <= 10:
+            print(eligible)
+        else:
+            print(not_eligible)
+    except ValueError:
+        print("Invalid health certificate number. Please enter a valid number.")
+
+else:
+    print(not_eligible)
