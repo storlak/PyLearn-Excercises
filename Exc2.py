75e7f56a6a7f0fe4cdb4d769d02e835f03d42a85
Kazure <serdartorlak@gmail.com>
2024-01-13 09:00:26 +0300

    Update

--- Exc2.py
+++ Exc2.py
@@ -1,11 +1,11 @@
-#Exc by Mosh: Converting lbs and kg.
+# Exc by Mosh: Converting lbs and kg.
 
 weight = int(input("Enter your weight : "))
 unit = input("(L)bs or (K)gs? ")
 
 if unit.upper == "L":
     converted = weight * 0.45
-    print(f"You're {converted} kgs.")
-else: 
+    print(f"You're {round(converted,1)} kgs.")
+else:
     converted = weight / 0.4589
-    print(f"You're {converted} lbs.")
+    print(f"You're {round(converted,1)} lbs.")
