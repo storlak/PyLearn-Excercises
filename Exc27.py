c1f2bacfae8a14a947e545c037f931812297d6b9
Kazure <serdartorlak@gmail.com>
2024-01-13 09:00:39 +0300

    Converting temperature

---
+++ Exc27.py
@@ -0,0 +1,11 @@
+temp = float(input("Enter the temperature: "))
+unit = input("Celcius(C) or Fahrenheit(F):").upper()
+
+if unit == "C":
+    temp = round(((9 * temp) / 5) + 32, 1)
+    print(f"Temperature in Fahrenheit is {temp}°F.")
+elif unit == "F":
+    temp = round(((temp - 32) * (5 / 9)), 1)
+    print(f"Temperature in Celcius is {temp}°C.")
+else:
+    print(f"{unit} is not a valid unit.")
