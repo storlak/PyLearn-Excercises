7ca29b75ef2307a3238017a3800fa38b3989683a
Kazure <serdartorlak@gmail.com>
2024-01-20 17:03:02 +0300

    Grocery Card

---
+++ Exc35.py
@@ -0,0 +1,24 @@
+foods = []
+prices = []
+total = 0
+
+# grocery card
+while True:
+    food = input("Enter a food to buy (q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
+
+print("----- YOUR CART -----")
+
+for food in foods:
+    print(food, end=" ")
+
+for price in prices:
+    total += price
+
+print()
+print(f"Your total is: ${total}")
