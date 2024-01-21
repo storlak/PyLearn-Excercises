15a1fe03ab148a39fd7e3981030f182c6f048ce0
Kazure <serdartorlak@gmail.com>
2024-01-21 18:38:09 +0300

    Guess a number

---
+++ Exc40.py
@@ -0,0 +1,18 @@
+import random
+
+low = 1
+high = 100
+guesses = 0
+number = random.randint(low, high)
+
+while True:
+    guess = int(input(f"Enter a number between {low} - {high}: "))
+    guesses += 1
+    if guess < number:
+        print(f"{guess} is too low.")
+    elif guess > number:
+        print(f"Guess is too high.")
+    else:
+        print(f"{guess} is correct!")
+        break
+print(f"This round took you {guesses} guesses.")
