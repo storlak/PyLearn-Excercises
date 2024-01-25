71665c6d109fa761457f28813f0de4f0c44d3ce5
Kazure <serdartorlak@gmail.com>
2024-01-25 20:38:39 +0300

    Encryption

---
+++ Exc41.py
@@ -0,0 +1,21 @@
+# encryption programme
+
+import random
+import string
+
+chars = " " + string.ascii_letters + string.punctuation + string.digits
+chars = list(chars)
+key = chars.copy()
+random.shuffle(key)
+
+print(f"chars:{chars}")
+print(f"key: {key}")
+
+# Encrypt
+plain_text = input("Enter your message to encrypt: ")
+cipher_text = ""
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+print(f"original message: {plain_text}")
+print(f"ciphered message: {cipher_text}")
