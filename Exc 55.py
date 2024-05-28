948a09a2ec52e2f70d393022cb976c6367ebb881
Kazure <serdartorlak@gmail.com>
2024-05-28 18:34:55 +0300

    assign variables & use f-string

--- Exc 55.py
+++ Exc 55.py
@@ -3,3 +3,12 @@
 
 price = 199.99
 print("This costs", price, "TL.")
+
+# Assign two variables that store the following values:
+"""$ 34.99 - product price (float)
+20 lbs - product weight (int)
+Using the f-string formatting style print to the console the following message:
+"""
+product_price = 34.99
+product_weight = 20
+print(f"Price: ${product_price}, Weight: {product_weight}lbs")
