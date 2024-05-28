6448e43e5e30d70ed48e5063c58dc67390317653
Kazure <serdartorlak@gmail.com>
2024-05-28 18:40:54 +0300

    # using print function "-"

---
+++ Exc 57.py
@@ -0,0 +1,11 @@
+"""
+Using three print() function (one line - one function) print the following text:
+----------------------------------------
+VERSION: 1.0.1
+----------------------------------------
+"""
+
+# Tip: The lines consist of 40 dash characters: '-'.
+print("-" * 40)
+print("VERSION: 1.0.1")
+print("-" * 40)
