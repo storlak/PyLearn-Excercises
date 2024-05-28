9c225e2a9a8dabe55bdff82e80ac65a1c75d946e
Kazure <serdartorlak@gmail.com>
2024-05-28 19:20:57 +0300

    using print function

---
+++ Exc 58.py
@@ -0,0 +1,15 @@
+# Using the four print() function (one line - one function) print the following text:'''
+# Tip: The lines consist of 40 equals signs: '='
+
+
+"""
+========================================
+author: johnsmith@sample.com
+date: 01-01-2021
+========================================
+"""
+
+print("=" * 40)
+print("author: johnsmith@sample.com")
+print("date: 01-01-2021")
+print("=" * 40)
