e8bef58000793427ab83d211e2f757947c36dcfb
Kazure <serdartorlak@gmail.com>
2024-05-28 17:37:50 +0300

    Use a variable and print function

---
+++ Exc 53.py
@@ -0,0 +1,6 @@
+# Assign to the variable age number 20.
+# Using the age variable and the print() function print to the console the following text:
+# I am 20 years old.
+
+age = 20
+print(f"I am {age} years old.")
