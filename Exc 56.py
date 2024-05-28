aeadbd9419a9adceb6f6e5451caf90860cb99cef
Kazure <serdartorlak@gmail.com>
2024-05-28 18:38:16 +0300

    rounding to 2 decimals

---
+++ Exc 56.py
@@ -0,0 +1,11 @@
+"""Below is an approximation of pi:
+pi = 3.1415926535
+Print the approximation of pi to two decimal places as shown below."""
+
+# Pi: 3.14
+
+pi = 3.1415926535
+print(f"{pi:.2f}")
+
+# or
+print(round(pi, 2))
