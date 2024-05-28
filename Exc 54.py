0d95522f9795579d2b9dca07ff1991e03e204ca9
Kazure <serdartorlak@gmail.com>
2024-05-28 17:41:35 +0300

    Using variables & print function & f-string

---
+++ Exc 54.py
@@ -0,0 +1,11 @@
+# Create two variables (you can freely choose the names) and assign to them following values:
+
+# 'Python'
+# '3.12'
+
+# Using these variables and the print() function, print to the console the following text:
+# I am learning Python version 3.8
+
+language = "Python"
+version = "3.12"
+print(f"I am learning {language} version {version}.")
