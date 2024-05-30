80e1b378ab638f6dc2fbdcd77f4c4fd708223668
Kazure <serdartorlak@gmail.com>
2024-05-30 14:27:55 +0300

    calculate the area of a circle

--- Exc 59.py
+++ Exc 59.py
@@ -3,3 +3,13 @@ radius = 5
 pi = 3.14
 area = pi * radius**2
 print(f"Area:{area:.2f}")
+
+# or
+
+print(f"Area:{pi * radius**2:.2f}")
+
+# or
+
+import math
+
+print(f"Area:{math.pi * radius**2:.2f}")
